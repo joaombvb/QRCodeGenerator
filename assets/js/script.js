36fabff744d6853f8715ed4031b0b3e0cc9d5afe
João Marcelo Barros Vilas Boas <joaombvb@gmail.com>
2025-03-14 19:03:46 -0300

    finaliza aplicação pra desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+const image = document.getElementById("imagem"); // Elemento da imagem do QR Code
+const inputError = document.getElementById("erro"); // Mensagem de erro do input
+const btnTxt = document.getElementById("btn-txt");// Mensagem dos botões
+
+
 // Aguarda o carregamento do DOM antes de adicionar eventos
 document.addEventListener("DOMContentLoaded", () => {
     const input = document.getElementById("input-link");
@@ -6,6 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
     input.addEventListener("focus", () => {
         input.style.backgroundImage = "url('../assets/images/link-100.png')";
         input.style.borderColor = "var(--gray-100)";
+        // Esconde a mensagem de erro
+        inputError.style.opacity = "0";
+
     });
 
     // Evento para restaurar o estilo quando perde o foco
@@ -18,18 +26,16 @@ document.addEventListener("DOMContentLoaded", () => {
 // Função para gerar o QR Code
 function gerarQrCode() {
     // Obtém os elementos do DOM
-    const link = document.getElementById("input-link").value.trim(); // Captura o valor do input e remove espaços extras
-    const image = document.getElementById("imagem"); // Elemento da imagem do QR Code
+    const inputLink = document.getElementById("input-link").value.trim(); // Captura o valor do input e remove espaços extras
     const imageText = document.getElementById("img-txt"); // Texto do espaço onde será exibido o QR Code
     const input = document.getElementById("input-link"); // Input onde o usuário digita o link
-    const inputError = document.getElementById("erro"); // Mensagem de erro
     const qrContainer = document.getElementById("qr-code"); // Container do QR Code
 
     // Verifica se o campo do link não está vazio
-    if (link) {
+    if (inputLink) {
         // Atualiza a imagem do QR Code com o link fornecido
-        image.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(link)}`;
-        image.alt = `QR Code para ${link}`; // Melhora a acessibilidade
+        image.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(inputLink)}`;
+        image.alt = `QR Code para ${inputLink}`; // Melhora a acessibilidade
 
         // Esconde o texto informativo da imagem
         imageText.style.display = "none";
@@ -42,12 +48,90 @@ function gerarQrCode() {
         input.style.border = "1px solid var(--gray-600)";
 
         // Esconde a mensagem de erro
-        inputError.style.display = "none";
+        inputError.style.opacity = "0";
         
     } else {
         // Se o campo estiver vazio, exibe erro visual e mensagem
         input.style.backgroundImage = "url('../assets/images/link-danger.png')";
         input.style.border = "1px solid var(--danger)";
-        inputError.style.display = "block";
+        inputError.style.opacity = "1";
     }
+}
+
+
+//Função para baixar a imagem do QR Code 
+function download() {
+
+    if (!image.src) {
+        btnError();
+        return;
+    }
+
+    // Usa o Fetch API para buscar a imagem
+    fetch(image.src)
+    .then(response => response.blob()) // Converte a resposta em um Blob
+    .then(blob => {
+        // Cria um link temporário
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob); // Cria uma URL para o Blob
+        link.download = "qrcode.jpg"; // Define o nome do arquivo para download
+        link.click(); // Simula o clique no link para iniciar o download
+
+        // Libera a URL do Blob da memória
+        URL.revokeObjectURL(link.href);
+
+        btnTxt.style.color = "var(--gray-200)";
+        btnTxt.textContent = "A imagem foi instalada com sucesso!";    
+        btnTxtFade();
+    })
+    .catch(error => {
+        console.error('Erro ao baixar a imagem:', error);
+    });
+}
+
+async function copy() {
+
+    if (!image.src) {
+        btnError(); 
+        return;
+    }
+
+    try {
+        // Converte a imagem em um Blob
+        const resposta = await fetch(imagem.src);
+        const blob = await resposta.blob();
+
+        // Copia o Blob para a área de transferência
+        await navigator.clipboard.write([
+            new ClipboardItem({
+                [blob.type]: blob
+            })
+        ]);
+
+        btnTxt.textContent = "Imagem copiada para área de transferência.";
+        btnTxt.style.color = "var(--gray-200)";
+        btnTxtFade();
+
+    } catch (error) {
+        console.error("Erro ao copiar a imagem:", error);
+    }
+}
+
+//Função para alertar o usuário em caso de erro nos botões
+function btnError() {
+    btnTxt.style.color = "var(--danger)";
+    btnTxt.textContent = "Nenhum código foi gerado. Por favor, tente novamente!";
+    btnTxtFade();
+}
+
+// Função para mostrar a mensagem dos botões de forma dinâmica
+function btnTxtFade() {
+    
+    // Aparece
+    btnTxt.classList.add('visible');
+
+    //Desaparece depois de 3s
+    setTimeout(() => {
+        btnTxt.classList.remove('visible');
+    }, 2000);
 }
